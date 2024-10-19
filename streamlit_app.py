1462e3856969eb60780688557ba646f8c1d927c1
giada.ehrlich <145689324+giadaehrlich002@users.noreply.github.com>
2024-10-19 17:42:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
+
 
 st.title('Visualisation Results of Cracks dataset❤️')
 st.info("this app shows the training results of the object detection on the cract dataset")
 st.write('Welcome')
+
+with st.expander('Data'): 
+  st.write('**Raw data**')
+  df = pd.read_csv('dataset')
+  
